3b121bbbac849b6bf9974eee1edf2df1f2a2338b
davidpokol <davidpokolol@gmail.com>
2024-02-25 16:05:38 +0100

    initial backend

---
+++ backend/src/main/java/com/vapo/completeit/CompleteItApplication.java
@@ -0,0 +1,13 @@
+package com.vapo.completeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteItApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/vapo/completeit/CompleteItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vapo.completeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
